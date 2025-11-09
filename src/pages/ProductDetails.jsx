660b4b04345785109c6b1e35c16ab802561722c8
MrPravinS <shegamwarpravin07@gmail.com>
2025-11-09 18:45:21 +0530

    add product details page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
-  return (
-    <div>ProductDetails</div>
-  )
-}
+  const [product, setProduct] = useState(null);
+  const { id } = useParams();
 
-export default ProductDetails
+  const fetchProduct = async () => {
+    const res = await fetch(`https://dummyjson.com/products/${id}`);
+    const data = await res.json();
+    setProduct(data);
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+  return (
+    <div className="flex justify-center border-2 border-purple-500/30">
+      <div className="card bg-base-100 w-96 shadow-sm ">
+        <figure>
+          <img src={product?.thumbnail} alt={product?.title} />
+        </figure>
+        <div className="card-body items-center">
+          <h2 className="card-title text-2xl font-medium  text-purple-400">
+            {product?.title}
+          </h2>
+          <h2 className="text-2xl font-semibold text-gray-400">
+            ${product?.price}
+          </h2>
+          <p>{product?.rating} / 5</p>
+          <p className="text-center">{product?.description}</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline btn btn-primary hover:shadow-lg hover:shadow-purple-500/50">
+              Add to Cart
+            </div>
+            <div className="badge badge-outline btn btn-primary hover:shadow-lg hover:shadow-purple-500/50">
+              Buy Now
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
